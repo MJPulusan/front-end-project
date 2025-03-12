2b2fe9cf18b3e0c0dccc33e255f1a2785ed8e02a
Michael Pulusan <mjpulusan@yahoo.com>
2025-03-12 22:26:48 +0000

    Ongoing CSS styling. #1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
+'use strict';
 document.addEventListener('DOMContentLoaded', () => {
   const $stateSelect = document.querySelector('#states');
-
   if (!$stateSelect) throw new Error('!$stateSelect does not exist.');
 });
