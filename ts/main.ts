1d416c511f9cd95f9068db0b806032824b4d8905
Michael Pulusan <mjpulusan@yahoo.com>
2025-03-13 00:05:42 +0000

    Figuring out how to pull api data. #1

--- ts/main.ts
+++ ts/main.ts
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const $stateSelect = document.querySelector('#states');
 
   if (!$stateSelect) throw new Error('!$stateSelect does not exist.');
+
+  // try {
+  // const response = await fetch(`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=tjsESTV0ZCq5TAudXkKvHgf2h8rRIqIkhHziRF5i`);
+  // const data = await response.json();
+
+  // } catch (error) {
+  //   console.log('Error:', error);
+  // }
 });
+
+async function fetchParks(sc: string): Promise<void> {
+  const api = 'tjsESTV0ZCq5TAudXkKvHgf2h8rRIqIkhHziRF5i';
+  const urlink = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${api}`;
+}
