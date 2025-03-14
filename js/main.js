11ccbd65bc80b3c551788bc530378a07516c1a64
Michael Pulusan <mjpulusan@yahoo.com>
2025-03-14 18:47:17 +0000

    Updated my park container placement. #1

--- js/main.js
+++ js/main.js
@@ -2,15 +2,23 @@
 const $apiKey = 'tjsESTV0ZCq5TAudXkKvHgf2h8rRIqIkhHziRF5i';
 const $stateSelect = document.querySelector('#states');
 const $parksContainer = document.createElement('div');
+const $overlay = document.querySelector('.overlay');
+const $overlay2 = document.querySelector('#overlay2');
 document.body.appendChild($parksContainer);
+$parksContainer.id = 'parksContainer';
 if (!$apiKey) throw new Error('!$apiKey is missing');
 if (!$stateSelect) throw new Error('!$stateSelect dropdown does not exist.');
 if (!$parksContainer) throw new Error('!$parksContainer does not exist.');
+if (!$overlay) throw new Error('!$overlay does not exist.');
+if (!$overlay2) throw new Error('!$overlay2 does not exist.');
 function getParkUrl(state) {
   return `https://developer.nps.gov/api/v1/parks?stateCode=${state}&api_key=${$apiKey}`;
 }
 async function fetchParks(state) {
-  if (!state) throw new Error('!state dropdown does not exist.');
+  if (!state) {
+    console.error('State is required.');
+    return;
+  }
   const url = getParkUrl(state);
   try {
     const response = await fetch(url);
@@ -20,6 +28,7 @@ async function fetchParks(state) {
     const info = await response.json();
     console.log(info);
     displayParks(info);
+    swapView();
   } catch (error) {
     console.error('Error fetching parks:', error);
   }
@@ -43,7 +52,11 @@ function displayParks(data) {
     }
   });
 }
+function swapView() {
+  $overlay.style.display = 'none';
+  $overlay2.style.display = 'flex';
+}
 // Event listener for dropdown changes
-$stateSelect.addEventListener('change', function () {
+$stateSelect.addEventListener('change', () => {
   fetchParks($stateSelect.value);
 });
